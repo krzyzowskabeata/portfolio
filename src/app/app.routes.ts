a00148d57e4e14ce7c51d438509b8347e9d82430
krzyzowskabeata <krzyzowska.beata@gmail.com>
2024-10-13 12:34:44 +0200

    Basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'welcome', component: HomeComponent, title: 'My portfolio' },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' } // ToDo: component: PageNotFoundComponent
+];
