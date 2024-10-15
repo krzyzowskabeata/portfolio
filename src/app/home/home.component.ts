ff2e2c6972e446bb4ca0f37a10fcd4f0ac5ed485
krzyzowskabeata <krzyzowska.beata@gmail.com>
2024-10-15 12:56:26 +0200

    Initial quote added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'pf-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  quote = 'A year spent in artificial intelligence is enough to make one believe in God.';
+  author = 'Alan Perlis';
 
 }
