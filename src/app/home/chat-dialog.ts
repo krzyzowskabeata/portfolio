8e1bcb3537087238e0af4ca2299d51c4f5faa0cc
krzyzowskabeata <krzyzowska.beata@gmail.com>
2024-10-30 15:35:01 +0100

    Chat as material diaog

---
+++ src/app/home/chat-dialog.ts
@@ -0,0 +1,7 @@
+export interface ChatDialog {
+    title: string;
+    subtitle: string;
+    message: string;
+    email: string;
+    linkedin: string;
+}
---
+++ src/app/home/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ChatDialog } from '../chat-dialog';
+
+@Component({
+  selector: 'pf-chat-dialog',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule, MatChipsModule, MatDialogModule],
+  templateUrl: './chat-dialog.component.html',
+  styleUrl: './chat-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChatDialogComponent {
+
+  readonly data = inject<ChatDialog>(MAT_DIALOG_DATA);
+
+}
